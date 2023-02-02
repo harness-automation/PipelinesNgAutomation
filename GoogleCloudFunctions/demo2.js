4c0d8cefda73d5d25086feb1d5b0747f981c59dd
Pragyesh Mishra <pragyesh.mishra@harness.io>
2023-02-02 21:01:02 +0530

    Update demo2.js

--- GoogleCloudFunctions/demo2.js
+++ GoogleCloudFunctions/demo2.js
@@ -5,17 +5,10 @@
             "runtime": "nodejs18",
             "entryPoint": "helloGET"
         },
-        "serviceConfig": {
-            "service": "",
-            "timeoutSeconds": 60,
-            "maxInstanceCount": 100,
-            "ingressSettings": "ALLOW_ALL",
-            "availableMemory": "256M",
-            "maxInstanceRequestConcurrency": 1,
-            "availableCpu": "0.1666",
-            "allTrafficOnLatestRevision": true
-        },
         "environment": "GEN_2"
     },
     "function_id": "nodejs-hello-world"
 }
+
+
+
