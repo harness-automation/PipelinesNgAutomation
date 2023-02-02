94c3515dfff73ea18011f8c19864bf5643fa052a
Pragyesh Mishra <pragyesh.mishra@harness.io>
2023-02-02 21:06:47 +0530

    Update demo2.js

--- GoogleCloudFunctions/demo2.js
+++ GoogleCloudFunctions/demo2.js
@@ -1,13 +1,13 @@
 {
     "function": {
-        "name": "nodejs-hello-world-canary",
+        "name": "<+stage.variables.functionName>",
         "buildConfig": {
             "runtime": "nodejs18",
             "entryPoint": "helloGET"
         },
         "environment": "GEN_2"
     },
-    "function_id": "nodejs-hello-world-canary"
+    "function_id": "<+stage.variables.functionName>"
 }
 
 
